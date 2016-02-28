37acffa2f31c60fca3fc65ef3bfceed685052934
funkymunky <ayesha.fernando@gmail.com>
2016-02-28 11:30:12 +1100

    Refactored ExpiredApplication out of ReportController class.

---
+++ src/main/java/net/lsf/site/report/ExpiredApplicationController.java
@@ -0,0 +1,68 @@
+package net.lsf.site.report;
+
+import net.lsf.BankInstitution;
+import net.lsf.InstitutionType;
+import net.lsf.SponsorshipType;
+import net.lsf.model.Student;
+import net.lsf.service.ReportService;
+import net.lsf.service.WriterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@Controller
+public class ExpiredApplicationController {
+
+    @Autowired
+    private ReportService reportService;
+
+    @Autowired
+    private WriterService writerService;
+
+    @RequestMapping(value = "/report/applicationExpired", method = RequestMethod.GET)
+    public String showStudentsWhereApplicationExpired(Model model) throws ReportException {
+        List<Student> students = reportService.getStudentsBySponsorshipType(SponsorshipType.ApplicationExpired);
+        model.addAttribute("students", students);
+        model.addAttribute("activeTab", "reportAE");
+        return "/reports/applicationExpired";
+    }
+
+    @RequestMapping(value = "/report/applicationExpired/school", method = RequestMethod.GET)
+    public String showOnlySchoolStudentsWhereApplicationExpired(Model model) throws ReportException {
+        List<Student> students = reportService.getStudentsByInstitutionType(SponsorshipType.ApplicationExpired, InstitutionType.School);
+        model.addAttribute("students", students);
+        model.addAttribute("activeTab", "reportAE");
+        return "/reports/applicationExpired";
+    }
+
+    @RequestMapping(value = "/report/applicationExpired/university", method = RequestMethod.GET)
+    public String showOnlyUniveristyStudentsWhereApplicationExpired(Model model) throws ReportException {
+        List<Student> students = reportService.getStudentsByInstitutionType(SponsorshipType.ApplicationExpired, InstitutionType.University);
+        model.addAttribute("students", students);
+        model.addAttribute("activeTab", "reportAE");
+        return "/reports/applicationExpired";
+    }
+
+    @RequestMapping(value="/report/applicationExpired/bank/{bankName}", method = RequestMethod.GET)
+    public String showOnlyApplicationExpiredWithBank(@PathVariable String bankName, Model model) throws ReportException {
+        BankInstitution bankInstitution = BankInstitution.getInstitutionFromString(bankName);
+        List<Student> students = reportService.getStudentsByBank(SponsorshipType.ApplicationExpired, bankInstitution);
+        model.addAttribute("students", students);
+        model.addAttribute("activeTab", "reportAE");
+        return "/reports/applicationExpired";
+    }
+
+    @RequestMapping(value="/report/applicationExpired/downloadCsv")
+    public void downloadCsvForStudentsWhereApplicationExpired(HttpServletResponse response) throws IOException, ReportException {
+        List<Student> listOfStudents = reportService.getStudentsBySponsorshipType(SponsorshipType.ApplicationExpired);
+        writerService.writeCsvFile("studentsApplicationExpired.csv", ReportController.HEADER_ROW, listOfStudents, response, false, false);
+    }
+
+}
--- src/main/java/net/lsf/site/report/ReportController.java
+++ src/main/java/net/lsf/site/report/ReportController.java
@@ -1,8 +1,5 @@
 package net.lsf.site.report;
 
-import net.lsf.AgentType;
-import net.lsf.InstitutionType;
-import net.lsf.SponsorshipType;
 import net.lsf.model.Student;
 import net.lsf.service.ReportService;
 import net.lsf.service.SponsorService;
@@ -12,7 +9,6 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -44,46 +40,6 @@ public class ReportController {
     public String listAllStudents(Model model) {
         return "/reports/list";
     }
-
-
-    @RequestMapping(value = "/report/applicationExpired", method = RequestMethod.GET)
-    public String showStudentsWhereApplicationExpired(Model model) throws ReportException {
-        List<Student> students = reportService.getStudentsBySponsorshipType(SponsorshipType.ApplicationExpired);
-        model.addAttribute("students", students);
-        model.addAttribute("activeTab", "reportAE");
-        return "/reports/applicationExpired";
-    }
-
-    @RequestMapping(value = "/report/applicationExpired/school", method = RequestMethod.GET)
-    public String showOnlySchoolStudentsWhereApplicationExpired(Model model) {
-        List<Student> students = studentService.getAllStudentsByInstitutionType(SponsorshipType.ApplicationExpired, InstitutionType.School);
-        model.addAttribute("students", students);
-        model.addAttribute("activeTab", "reportAE");
-        return "/reports/applicationExpired";
-    }
-
-    @RequestMapping(value = "/report/applicationExpired/university", method = RequestMethod.GET)
-    public String showOnlyUniveristyStudentsWhereApplicationExpired(Model model) {
-        List<Student> students = studentService.getAllStudentsByInstitutionType(SponsorshipType.ApplicationExpired, InstitutionType.University);
-        model.addAttribute("students", students);
-        model.addAttribute("activeTab", "reportAE");
-        return "/reports/applicationExpired";
-    }
-
-    @RequestMapping(value="/report/applicationExpired/bank/{bankName}", method = RequestMethod.GET)
-    public String showOnlyApplicationExpiredWithBank(@PathVariable String bankName, Model model) {
-        List<Student> students = studentService.getAllStudentsWithBank(SponsorshipType.ApplicationExpired, bankName);
-        model.addAttribute("students", students);
-        model.addAttribute("activeTab", "reportAE");
-        return "/reports/applicationExpired";
-    }
-
-    @RequestMapping(value="/report/applicationExpired/downloadCsv")
-    public void downloadCsvForStudentsWhereApplicationExpired(HttpServletResponse response) throws IOException, ReportException {
-        List<Student> listOfStudents = reportService.getStudentsBySponsorshipType(SponsorshipType.ApplicationExpired);
-        writerService.writeCsvFile("studentsApplicationExpired.csv", HEADER_ROW, listOfStudents, response, false, false);
-    }
-
 
     @RequestMapping(value = "/report/allStudents", method = RequestMethod.GET)
     public String showAllStudents(Model model) {
